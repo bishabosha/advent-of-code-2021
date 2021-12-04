e4d24012e93e091e09fcea4409657d878faf61b2
Jamie Thompson <bishbashboshjt@gmail.com>
2021-12-04 11:48:44 +0100

    add day 2 in haskell

--- haskell/runner/Main.hs
+++ haskell/runner/Main.hs
@@ -2,10 +2,15 @@ module Main where
 
 import qualified Advent (runChallenge, readLinesAsInt)
 import qualified Day01 (part1, part2)
+import qualified Day02 (parseCommands, part1, part2)
 
 day01 = Advent.runChallenge "day01" Advent.readLinesAsInt
+day02 = Advent.runChallenge "day02" (Day02.parseCommands . lines)
 
 main :: IO ()
 main = do
   day01 "part1" Day01.part1
   day01 "part2" Day01.part2
+  day02 "part1" Day02.part1
+  day02 "part2" Day02.part2
+
