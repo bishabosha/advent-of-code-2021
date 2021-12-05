19d758ce0c9430e30569794688fc6d9ad1e32894
Jamie Thompson <bishbashboshjt@gmail.com>
2021-12-05 10:14:24 +0100

    day 3 part 2 in haskell

--- haskell/runner/Main.hs
+++ haskell/runner/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import qualified Advent (runChallenge, readLinesAsInt)
 import qualified Day01 (part1, part2)
 import qualified Day02 (parseCommands, part1, part2)
-import qualified Day03 (part1)
+import qualified Day03 (part1, part2)
 
 day01 = Advent.runChallenge "day01" Advent.readLinesAsInt
 day02 = Advent.runChallenge "day02" (Day02.parseCommands . lines)
@@ -16,4 +16,5 @@ main = do
   day02 "part1" Day02.part1
   day02 "part2" Day02.part2
   day03 "part1" Day03.part1
+  day03 "part2" Day03.part2
 
