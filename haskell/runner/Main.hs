c31bddb418cd4f13647a196e88e4f0f83f813164
Jamie Thompson <bishbashboshjt@gmail.com>
2021-12-05 14:10:37 +0100

    day 4 parts 1 and 2 in haskell

--- haskell/runner/Main.hs
+++ haskell/runner/Main.hs
@@ -4,10 +4,12 @@ import qualified Advent (runChallenge, readLinesAsInt)
 import qualified Day01 (part1, part2)
 import qualified Day02 (parseCommands, part1, part2)
 import qualified Day03 (part1, part2)
+import qualified Day04 (parseBingoGame, part1, part2)
 
 day01 = Advent.runChallenge "day01" Advent.readLinesAsInt
 day02 = Advent.runChallenge "day02" (Day02.parseCommands . lines)
 day03 = Advent.runChallenge "day03" (Just . lines)
+day04 = Advent.runChallenge "day04" (Day04.parseBingoGame . lines)
 
 main :: IO ()
 main = do
@@ -17,4 +19,6 @@ main = do
   day02 "part2" Day02.part2
   day03 "part1" Day03.part1
   day03 "part2" Day03.part2
+  day04 "part1" Day04.part1
+  day04 "part2" Day04.part2
 
